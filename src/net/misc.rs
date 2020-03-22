b95eb11c3b1d78174fa0b5f29d34e767c81b4b2b
Bob Matcuk <bmatcuk@gmail.com>
2020-03-22 11:54:05 -0400

    finished misc net functions

--- src/net/misc.rs
+++ src/net/misc.rs
@@ -1,9 +1,5 @@
-use crate::{FromInner, IntoInner};
 use std::ffi::CStr;
-use uv::{
-    uv_if_indextoiid, uv_if_indextoname, uv_interface_addresses,
-    uv_ip4_addr, uv_ip4_name, uv_ip6_addr, uv_ip6_name, UV_IF_NAMESIZE,
-};
+use uv::{uv_if_indextoiid, uv_if_indextoname, uv_interface_addresses, UV_IF_NAMESIZE};
 
 /// IPv6-capable implementation of if_indextoname(3).
 ///
@@ -17,16 +13,15 @@ use uv::{
 pub fn if_indextoname(ifindex: u32) -> crate::Result<String> {
     let mut size = UV_IF_NAMESIZE as usize;
     let mut buf: Vec<std::os::raw::c_uchar> = Vec::with_capacity(size);
-    crate::uvret(unsafe {
-        uv_if_indextoname(ifindex, buf.as_mut_ptr() as _, &mut size as _)
-    })
-    .map(|_| {
-        // size is the length of the string, *not* including the null
-        unsafe { buf.set_len(size + 1) };
-        unsafe { CStr::from_bytes_with_nul_unchecked(&buf) }
-            .to_string_lossy()
-            .into_owned()
-    })
+    crate::uvret(unsafe { uv_if_indextoname(ifindex, buf.as_mut_ptr() as _, &mut size as _) }).map(
+        |_| {
+            // size is the length of the string, *not* including the null
+            unsafe { buf.set_len(size + 1) };
+            unsafe { CStr::from_bytes_with_nul_unchecked(&buf) }
+                .to_string_lossy()
+                .into_owned()
+        },
+    )
 }
 
 /// Retrieves a network interface identifier suitable for use in an IPv6 scoped address. On
@@ -37,18 +32,18 @@ pub fn if_indextoname(ifindex: u32) -> crate::Result<String> {
 pub fn if_indexto_iid(ifindex: u32) -> crate::Result<String> {
     let mut size = UV_IF_NAMESIZE as usize;
     let mut buf: Vec<std::os::raw::c_uchar> = Vec::with_capacity(size);
-    crate::uvret(unsafe {
-        uv_if_indextoiid(ifindex, buf.as_mut_ptr() as _, &mut size as _)
-    })
-    .map(|_| {
-        // size is the length of the string, *not* including the null
-        unsafe { buf.set_len(size + 1) };
-        unsafe { CStr::from_bytes_with_nul_unchecked(&buf) }
-            .to_string_lossy()
-            .into_owned()
-    })
+    crate::uvret(unsafe { uv_if_indextoiid(ifindex, buf.as_mut_ptr() as _, &mut size as _) }).map(
+        |_| {
+            // size is the length of the string, *not* including the null
+            unsafe { buf.set_len(size + 1) };
+            unsafe { CStr::from_bytes_with_nul_unchecked(&buf) }
+                .to_string_lossy()
+                .into_owned()
+        },
+    )
 }
 
+/// Gets address information about the network interfaces on the system.
 pub fn interface_addresses() -> crate::Result<crate::InterfaceAddressesIter> {
     let mut addresses: *mut uv::uv_interface_address_t = std::mem::zeroed();
     let mut count: std::os::raw::c_int = 0;
