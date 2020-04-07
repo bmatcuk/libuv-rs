893828cb8a62f5de56f685284355ffa75c4761f6
Bob Matcuk <bmatcuk@gmail.com>
2020-04-06 21:58:34 -0400

    tcp-echo-server almost works - sockaddr_in is not in network byteorder

---
+++ examples/tcp-echo-server.rs
@@ -0,0 +1,69 @@
+extern crate libuv;
+use libuv::prelude::*;
+use libuv::{Buf, TcpBindFlags, ReadonlyBuf};
+use std::net::Ipv4Addr;
+
+const DEFAULT_PORT: u16 = 7000;
+const DEFAULT_BACKLOG: i32 = 128;
+
+fn alloc_buffer(_: Handle, suggested_size: usize) -> Option<Buf> {
+    Buf::with_capacity(suggested_size).ok()
+}
+
+fn echo_write(mut buf: ReadonlyBuf, status: libuv::Result<i32>) {
+    if let Err(e) = status {
+        eprintln!("Write error {}", e);
+    }
+    buf.dealloc();
+}
+
+fn echo_read(mut client: StreamHandle, nread: libuv::Result<isize>, buf: ReadonlyBuf) {
+    match nread {
+        Ok(len) => {
+            if len > 0 {
+                if let Err(e) = client.write(&[buf], Some(move |_, s| echo_write(buf, s))) {
+                    eprintln!("Error echoing to socket: {}", e);
+                }
+            }
+        },
+        Err(e) => {
+            if e != libuv::Error::EOF {
+                eprintln!("Read error {}", e);
+            }
+            client.close(None::<fn(Handle)>);
+        }
+    }
+}
+
+fn on_new_connection(mut server: StreamHandle, status: libuv::Result<i32>) {
+    if let Err(e) = status {
+        eprintln!("New connection error: {}", e);
+        return;
+    }
+
+    println!("New connection!");
+    if let Ok(client) = server.get_loop().tcp().as_mut() {
+        match server.accept(&mut client.to_stream()) {
+            Ok(_) => if let Err(e) = client.read_start(Some(alloc_buffer), Some(echo_read)) {
+                eprintln!("Error starting read on client: {}", e);
+            },
+            Err(e) => {
+                eprintln!("Error accepting connection: {}", e);
+                client.close(None::<fn(Handle)>)
+            },
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut r#loop = Loop::default()?;
+
+    let mut server = r#loop.tcp()?;
+    let addr = (Ipv4Addr::LOCALHOST, DEFAULT_PORT).into();
+    server.bind(&addr, TcpBindFlags::empty())?;
+    server.listen(DEFAULT_BACKLOG, Some(on_new_connection))?;
+
+    r#loop.run(RunMode::Default)?;
+
+    Ok(())
+}
