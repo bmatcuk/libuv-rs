0e55dfbbc2737d3539b6eb8b022f6f6458cd5665
Bob Matcuk <bmatcuk@gmail.com>
2020-05-09 09:13:16 -0400

    finished progress example

--- examples/progress.rs
+++ examples/progress.rs
@@ -1,13 +1,15 @@
 extern crate libuv;
 use libuv::prelude::*;
-use libuv::{AsyncHandle};
+use libuv::AsyncHandle;
+use std::cell::Cell;
+use std::rc::Rc;
 
 const SIZE: usize = 10240;
 
-fn fake_download(notify: &mut AsyncHandle, progress: &mut f32) {
+fn fake_download(notify: &mut AsyncHandle, progress: &Rc<Cell<f32>>) {
     let mut downloaded = 0usize;
     while downloaded < SIZE {
-        *progress = (downloaded as f32) * 100.0 / (SIZE as f32);
+        progress.replace((downloaded as f32) * 100.0 / (SIZE as f32));
         if let Err(e) = notify.send() {
             eprintln!("Failed to send async notification {}", e);
         }
@@ -18,19 +20,23 @@ fn fake_download(notify: &mut AsyncHandle, progress: &mut f32) {
 
 fn after(notify: &mut AsyncHandle) {
     println!("Download complete");
-    let _ = notify.close(None::<fn(_)>);
+    let _ = notify.close(());
 }
 
-fn print_progress(progress: &f32) {
-    println!("Downloaded {}%", progress);
+fn print_progress(progress: &Rc<Cell<f32>>) {
+    println!("Downloaded {}%", progress.get());
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut r#loop = Loop::default()?;
 
-    let mut progress = 0f32;
-    let mut notify = r#loop.r#async(Some(|_| print_progress(&progress)))?;
-    r#loop.queue_work(Some(|_| fake_download(&mut notify, &mut progress)), Some(|_, _| after(&mut notify)))?;
+    let progress = Rc::new(Cell::new(0f32));
+    let progress2 = progress.clone();
+    let mut notify = r#loop.r#async(move |_| print_progress(&progress))?;
+    r#loop.queue_work(
+        move |_| fake_download(&mut notify, &progress2),
+        move |_, _| after(&mut notify),
+    )?;
 
     r#loop.run(RunMode::Default)?;
 
