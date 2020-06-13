d70fbc69f3118e7daf399df1cfb4510c2ef2544c
Bob Matcuk <bmatcuk@gmail.com>
2020-06-13 07:04:31 -0400

    tty example

---
+++ examples/tty.rs
@@ -0,0 +1,24 @@
+extern crate libuv;
+use libuv::prelude::*;
+use libuv::{Buf, HandleType, TtyHandle, TtyMode, guess_handle};
+
+const STDOUT: libuv::File = 1;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut r#loop = Loop::default()?;
+
+    let mut tty = r#loop.tty(STDOUT)?;
+    tty.set_mode(TtyMode::Normal)?;
+    if (guess_handle(STDOUT) == HandleType::TTY) {
+        let buf = Buf::new("\x1b[41;37m")?;
+        tty.write(&[buf], ())?;
+    }
+
+    let buf = Buf::new("Hello TTY\n")?;
+    tty.write(&[buf], ())?;
+    TtyHandle::reset_mode()?;
+
+    r#loop.run(RunMode::Default)?;
+
+    Ok(())
+}
